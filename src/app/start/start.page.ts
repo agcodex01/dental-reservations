31d649297b6d4244773c043d4a7c6643aa8a0935
loremZyn14 <yasuaimo@gmail.com>
2022-06-16 00:34:48 +0800

    update

--- src/app/start/start.page.ts
+++ src/app/start/start.page.ts
@@ -1,6 +1,11 @@
 import { Router } from '@angular/router';
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormControl, FormGroup } from '@angular/forms';
+import {
+  FormBuilder,
+  FormControl,
+  FormGroup,
+  Validators,
+} from '@angular/forms';
 
 @Component({
   selector: 'app-start',
@@ -12,19 +17,17 @@ export class StartPage implements OnInit {
   infoGroup: FormGroup;
   constructor(private router: Router, public fb: FormBuilder) {
     this.infoGroup = this.fb.group({
-      name: new FormControl(''),
+      name: new FormControl('', [Validators.required]),
       preferContact: new FormControl('email'),
-      contactData: new FormControl('')
+      contactData: new FormControl('', [Validators.required]),
     });
   }
 
-  ngOnInit() {
-  }
+  ngOnInit() {}
 
   start() {
-    console.log(this.infoGroup.value);
+    console.log(this.infoGroup);
     localStorage.setItem('info', JSON.stringify(this.infoGroup.value));
     this.router.navigate(['tabs/tab1']);
   }
-
 }
